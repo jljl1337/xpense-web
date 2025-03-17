8d343029c1a6c6d62831ca81a87cb49380d1d8ca
jljl1337 <lckjack123@gmail.com>
2025-03-16 22:57:18 -0600

    refactor: remove unused variable and unify formatting

--- src/lib/db/server.ts
+++ src/lib/db/server.ts
@@ -18,7 +18,7 @@ export async function createClient() {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
+              cookieStore.set(name, value, options),
             );
           } catch {
             // The `setAll` method was called from a Server Component.
@@ -27,6 +27,6 @@ export async function createClient() {
           }
         },
       },
-    }
+    },
   );
 }
