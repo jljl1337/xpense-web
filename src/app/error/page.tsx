ba3e90803a78ecc46ae968aeae9fe5542ae0cc54
jljl1337 <lckjack123@gmail.com>
2025-03-21 18:19:29 -0600

    feat: add error page component with user-friendly message

---
+++ src/app/error/page.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      <h1 className="text-4xl">Something went wrong...</h1>
+    </div>
+  );
+}
