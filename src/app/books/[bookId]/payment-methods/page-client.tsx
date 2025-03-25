c05c086324f7ee5a8d5e7456a17f1048833831be
jljl1337 <43352209+jljl1337@users.noreply.github.com>
2025-03-25 04:52:03 -0600

    feat: add payment method CRUD (#12)
    * feat: add PaymentMethod type to database types
    
    * feat: add payment method dal and server actions
    
    * feat: add payment methods management pages
    
    * chore: bump version to 2.4.0

---
+++ src/app/books/[bookId]/payment-methods/page-client.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import Link from "next/link";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+
+import { DataTable } from "@/components/data-table";
+import TableRowDropdown from "@/components/table-row-dropdown";
+import { deletePaymentMethod } from "@/lib/actions/payment-methods";
+import { PaymentMethod } from "@/lib/db/types";
+import { formatDateFromISO } from "@/lib/formats/date";
+
+interface PaymentMethodsClientPageProps {
+  bookId: string;
+  paymentMethods: PaymentMethod[];
+}
+
+const columns: ColumnDef<PaymentMethod>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => {
+      return (
+        <Link
+          href={`/books/${row.original.book_id}/payment-methods/${row.original.id}/edit`}
+        >
+          {row.original.name}
+        </Link>
+      );
+    },
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  {
+    accessorKey: "created_at",
+    header: "Created At",
+    cell: ({ row }) => {
+      const formatted = formatDateFromISO(row.original.created_at);
+
+      return <div>{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const deleteAction = async () => {
+        return deletePaymentMethod({ id: row.original.id });
+      };
+
+      return (
+        <TableRowDropdown
+          editUrl={`/books/${row.original.book_id}/payment-methods/${row.original.id}/edit`}
+          deleteAction={deleteAction}
+        />
+      );
+    },
+  },
+];
+
+export default function PaymentMethodsClientPage({
+  bookId,
+  paymentMethods,
+}: PaymentMethodsClientPageProps) {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <div className="h-full max-w-[120rem] flex-1 flex flex-col p-8 gap-4">
+        <h1 className="text-4xl">Payment Methods</h1>
+        <Button className="w-24" asChild>
+          <Link href={`/books/${bookId}/payment-methods/create`}>Create</Link>
+        </Button>
+        <DataTable columns={columns} data={paymentMethods} />
+      </div>
+    </div>
+  );
+}
--- src/lib/db/types.ts
+++ src/lib/db/types.ts
@@ -2,3 +2,5 @@ import { Database } from "@/lib/db/types.g";
 
 export type Book = Database["xpense"]["Tables"]["book"]["Row"];
 export type Category = Database["xpense"]["Tables"]["category"]["Row"];
+export type PaymentMethod =
+  Database["xpense"]["Tables"]["payment_method"]["Row"];
