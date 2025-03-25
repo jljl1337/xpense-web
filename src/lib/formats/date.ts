227b6593eb856add5857fa519a141329cb47b77f
jljl1337 <lckjack123@gmail.com>
2025-03-25 06:42:53 -0600

    feat: add function to format date from ISO string

--- src/lib/formats/date.ts
+++ src/lib/formats/date.ts
@@ -3,3 +3,7 @@ import { DateTime } from "luxon";
 export function formatDateTimeFromISO(isoString: string): string {
   return DateTime.fromISO(isoString).toFormat("yyyy-MM-dd HH:mm:ss");
 }
+
+export function formatDateFromISO(isoString: string): string {
+  return isoString.substring(0, 10);
+}
