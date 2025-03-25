b92c7b7131c6ee7b6b8283084fbfc6dba0f0585e
jljl1337 <lckjack123@gmail.com>
2025-03-25 07:07:58 -0600

    feat: implement book dashboard page

---
+++ src/app/books/[bookId]/page-client.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import Link from "next/link";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Button } from "@/components/ui/button";
+
+import { DataTable } from "@/components/data-table";
+import TableRowDropdown from "@/components/table-row-dropdown";
+import { deleteExpense } from "@/lib/actions/expenses";
+import { Category, Expense, PaymentMethod } from "@/lib/db/types";
+import { formatDateFromISO, formatDateTimeFromISO } from "@/lib/formats/date";
+
+interface BookDashboardClientPageProps {
+  bookId: string;
+  categories: Category[];
+  paymentMethods: PaymentMethod[];
+  expenses: Expense[];
+}
+
+export default function BookDashboardClientPage({
+  bookId,
+  categories,
+  paymentMethods,
+  expenses,
+}: BookDashboardClientPageProps) {
+  const columns: ColumnDef<Expense>[] = [
+    {
+      accessorKey: "category_id",
+      header: "Category",
+      cell: ({ row }) => {
+        return (
+          <Link
+            href={`/books/${row.original.book_id}/categories/${row.original.category_id}/edit`}
+          >
+            {
+              categories.find(
+                (category) => category.id === row.original.category_id,
+              )!.name
+            }
+          </Link>
+        );
+      },
+    },
+    {
+      accessorKey: "payment_method_id",
+      header: "Payment Method",
+      cell: ({ row }) => {
+        return (
+          <Link
+            href={`/books/${row.original.book_id}/payment-methods/${row.original.payment_method_id}/edit`}
+          >
+            {
+              paymentMethods.find(
+                (method) => method.id === row.original.payment_method_id,
+              )!.name
+            }
+          </Link>
+        );
+      },
+    },
+    {
+      accessorKey: "date",
+      header: "Date",
+      cell: ({ row }) => {
+        const formatted = formatDateFromISO(row.original.date);
+
+        return <div>{formatted}</div>;
+      },
+    },
+    {
+      accessorKey: "amount",
+      header: "Amount",
+    },
+    {
+      accessorKey: "remark",
+      header: "Remark",
+    },
+    {
+      accessorKey: "created_at",
+      header: "Created At",
+      cell: ({ row }) => {
+        const formatted = formatDateTimeFromISO(row.original.created_at);
+
+        return <div>{formatted}</div>;
+      },
+    },
+    {
+      accessorKey: "updated_at",
+      header: "Updated At",
+      cell: ({ row }) => {
+        const formatted = formatDateTimeFromISO(row.original.updated_at);
+
+        return <div>{formatted}</div>;
+      },
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const deleteAction = async () => {
+          return deleteExpense({ id: row.original.id });
+        };
+
+        return (
+          <TableRowDropdown
+            editUrl={`/books/${row.original.book_id}/expenses/${row.original.id}/edit`}
+            deleteAction={deleteAction}
+          />
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="h-full flex items-center justify-center">
+      <div className="h-full max-w-[120rem] flex-1 flex flex-col p-8 gap-4">
+        <h1 className="text-4xl">Dashboard</h1>
+        <Button className="w-24" asChild>
+          <Link href={`/books/${bookId}/expenses/create`}>Create</Link>
+        </Button>
+        <DataTable columns={columns} data={expenses} />
+      </div>
+    </div>
+  );
+}
--- src/app/books/[bookId]/page.tsx
+++ src/app/books/[bookId]/page.tsx
@@ -1,12 +1,41 @@
-export default async function BookPage({
+import { redirect } from "next/navigation";
+
+import BookDashboardClientPage from "@/app/books/[bookId]/page-client";
+import { getCategories } from "@/lib/db/categories";
+import { getExpenses } from "@/lib/db/expenses";
+import { getPaymentMethods } from "@/lib/db/payment-methods";
+
+export default async function BookDashboardPage({
   params,
 }: {
   params: Promise<{ bookId: string }>;
 }) {
   const { bookId } = await params;
+
+  const categoriesPromise = getCategories(bookId);
+  const paymentMethodsPromise = getPaymentMethods(bookId);
+  const expensesPromise = getExpenses(bookId);
+
+  const [
+    { data: categories, error: categoriesError },
+    { data: paymentMethods, error: paymentMethodsError },
+    { data: expenses, error: expensesError },
+  ] = await Promise.all([
+    categoriesPromise,
+    paymentMethodsPromise,
+    expensesPromise,
+  ]);
+
+  if (categoriesError || paymentMethodsError || expensesError) {
+    redirect("/error");
+  }
+
   return (
-    <div className="h-full w-full flex items-center justify-center">
-      Book: {bookId}
-    </div>
+    <BookDashboardClientPage
+      bookId={bookId}
+      categories={categories!}
+      paymentMethods={paymentMethods!}
+      expenses={expenses!}
+    />
   );
 }
