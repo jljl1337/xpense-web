d33f0d33b37f0892d8194516673c83d48ef64eac
jljl1337 <lckjack123@gmail.com>
2025-04-16 04:26:00 -0600

    feat: replace landing page text with logo image in Header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 import { Separator } from "@/components/ui/separator";
@@ -12,10 +13,16 @@ export async function Header() {
   return (
     <header className="flex-none flex flex-col">
       <nav className="flex flex-1 items-center justify-between m-3">
-        {/* Landing page text */}
         <div className="flex items-center gap-9">
           <Link href="/">
-            <h1 className="text-2xl">Xpense</h1>
+            <Image
+              className="invert dark:invert-0"
+              src="/icon.png"
+              alt="logo"
+              width={38}
+              height={38}
+              priority
+            />
           </Link>
           <Link href="/books">
             <h2 className="text-xl">Books</h2>
