4ab0b9d0802a3b3d46d4af3e4c033b540361c65b
jljl1337 <lckjack123@gmail.com>
2025-04-16 04:33:49 -0600

    feat: add book name next to sidebar trigger

--- src/app/books/[bookId]/layout.tsx
+++ src/app/books/[bookId]/layout.tsx
@@ -1,3 +1,6 @@
+import { redirect } from "next/navigation";
+
+import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
@@ -5,6 +8,7 @@ import {
 } from "@/components/ui/sidebar";
 
 import { BookSidebar } from "@/components/sidebar";
+import { getBooks } from "@/lib/db/books";
 
 export default async function Layout({
   children,
@@ -15,11 +19,21 @@ export default async function Layout({
 }) {
   const { bookId } = await params;
 
+  const { data: books, error } = await getBooks({ id: bookId });
+
+  if (error) {
+    redirect("/error");
+  }
+
   return (
     <SidebarProvider>
       <BookSidebar bookId={bookId} />
       <SidebarInset>
-        <SidebarTrigger className="mx-2 my-2" />
+        <div className="mx-2 my-2 flex items-center">
+          <SidebarTrigger />
+          <Separator orientation="vertical" className="mx-2 h-4" />
+          <div>{books[0].name}</div>
+        </div>
         {children}
       </SidebarInset>
     </SidebarProvider>
