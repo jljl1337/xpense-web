fc838c6f503ab79632df5f66510a136f712b1dcb
jljl1337 <lckjack123@gmail.com>
2025-04-17 01:45:21 -0600

    feat: add PieChartCard and TotalByGroupPieChartCard components for visualizing expenditure data

---
+++ src/components/pie-chart-card.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Pie, PieChart } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { cn } from "@/lib/utils";
+
+interface PieChartCardProps {
+  title?: string;
+  description?: string;
+  chartData: Record<string, any>[];
+  chartConfig: ChartConfig;
+  dataKey: string;
+  nameKey: string;
+  className?: string;
+}
+
+export function PieChartCard({
+  title,
+  description,
+  chartData,
+  chartConfig,
+  dataKey,
+  nameKey,
+  className,
+}: PieChartCardProps) {
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className={cn("mx-auto aspect-square", className)}
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey={dataKey}
+              nameKey={nameKey}
+              startAngle={90}
+              endAngle={-270}
+            />
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/components/total-by-group-pie-chart-card.tsx
@@ -0,0 +1,50 @@
+import { redirect } from "next/navigation";
+
+import { PieChartCard } from "./pie-chart-card";
+
+import { getTotalByGroup } from "@/lib/db/summary";
+
+interface TotalByGroupPieChartCardProps {
+  bookId: string;
+  days?: number;
+}
+
+export default async function TotalByGroupPieChartCard({
+  bookId,
+  days,
+}: TotalByGroupPieChartCardProps) {
+  const { data: totalByGroup, error } = await getTotalByGroup({
+    bookId,
+    days,
+  });
+
+  if (error) {
+    redirect("/error");
+  }
+
+  const chartConfig = totalByGroup.reduce((acc: any, item: any) => {
+    acc[item["id"]] = {
+      label: item["name"],
+      color: `var(--chart-${(Object.keys(acc).length + 1) % 12})`,
+    };
+    return acc;
+  }, {});
+
+  const chartData = totalByGroup.map((item: any) => ({
+    id: item["id"],
+    value: item["total_amount"],
+    fill: `var(--color-${item["id"]})`,
+  }));
+
+  return (
+    <PieChartCard
+      title="Total by Category"
+      description={`Expenditure of ${days ? `the last ${days}` : "all"} days`}
+      chartData={chartData}
+      chartConfig={chartConfig}
+      dataKey={"value"}
+      nameKey={"id"}
+      className="h-48"
+    />
+  );
+}
